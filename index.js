031de18b1a3576327e3dfd7b8b0a936d5eed435b
EngrMunir <smsirajulmonir@gmail.com>
2024-06-15 01:47:35 -0700

    book post api installed

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 const app=express();
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://<username>:<password>@cluster0.yk1xelo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +23,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+     // const userCollection = client.db('juteDB').collection('user');
+     const booksCollection = client.db('libraryDb').collection('Books');
+     const categoryCollection = client.db('libraryDb').collection('Category');
+     const borrowCollection = client.db('libraryDb').collection('Borrowed');
+     const userCollection = client.db('libraryDb').collection('Users');
+ 
+// books related api
+app.get('/books',logger, verifyToken, async(req, res)=>{
+    // console.log('token owner info :',req.user)
+    const books = await booksCollection.find().toArray();
+    res.send(books);
+  })
+  
+  app.post('/books',verifyToken, async(req, res) =>{
+    const book = req.body;
+    // Convert quantity and rating to integers
+    book.quantity = parseInt(book.quantity);
+    book.rating = parseInt(book.rating);
+    const result = await booksCollection.insertOne(book);
+    res.send(result);
+  })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
